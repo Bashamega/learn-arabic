2e159d80c828bbd181d34ec7f4ee1671a9353a3c
Bashamega <adambashaahmednaji@gmail.com>
2024-05-10 07:43:19 +0300

    Vercel Error

--- src/components/notFound.tsx
+++ src/components/notFound.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 export function NotFound() {
     return (
@@ -80,12 +81,12 @@ export function NotFound() {
             <div className="flex flex-col items-center justify-center">
                 <p className="text-3xl md:text-4xl lg:text-5xl text-gray-800 mt-12"> الصفحة غير موجودة</p>
                 <p className="md:text-lg lg:text-xl text-gray-600 mt-8">عذرا، الصفحة التي تبحث عنها لا يمكن العثور عليها.</p>
-                <a href="/" className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 mt-12 rounded transition duration-150" title="Return Home">
+                <Link href="/" className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 mt-12 rounded transition duration-150" title="Return Home">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path>
                     </svg>
                     <span>العودة إلى المنزل</span>
-                </a>
+                </Link>
             </div>
         </div>
     )
